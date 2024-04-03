d550d1fd3ee1182869883166bbbb6b6eb27a8ad7
VitorLimaGit <vitorliima18@gmail.com>
2024-04-02 22:50:42 -0300

    feature/SearchByKeyword: Implement method for search by keyword

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit{
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute){
@@ -26,10 +27,35 @@ export class ProductListComponent implements OnInit{
 
   listProducts() {
 
-  // check if "id" parameter is available
-  const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
-  
-     if (hasCategoryId) {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if(this.searchMode){
+      this.handleSearchProducts();
+    } else{
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+
+  handleListProducts() {
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     }
@@ -40,9 +66,9 @@ export class ProductListComponent implements OnInit{
 
     // now get the products for the given category id
     this.productService.getProductList(this.currentCategoryId).subscribe(
-      data =>{
+      data => {
         this.products = data;
       }
-    );
+    )    
   }
 }
