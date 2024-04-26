b6b718da48c607dd7cadb6884a2f2108e01cac91
VitorLima <vitorliima18@gmail.com>
2024-04-26 15:16:31 -0300

    feature/CheckoutFormLayout: Added Billing Address and implement box to copy from Shipping Address

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -17,37 +17,46 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
 
     this.checkoutFormGroup = this.formBuilder.group({
-
       customer: this.formBuilder.group({
-
         firstName: [''],
         lastName: [''],
         email: ['']
-
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
         city: [''],
         state: [''],
         country: [''],
-        zipCode: [''],
+        zipCode: ['']
       }),
       billingAddress: this.formBuilder.group({
         street: [''],
         city: [''],
         state: [''],
         country: [''],
-        zipCode: [''],
+        zipCode: ['']
       }),
-      creditCart: this.formBuilder.group({
+      creditCard: this.formBuilder.group({
         cardType: [''],
         nameOnCard: [''],
-        creditNumber: [''],
+        cardNumber: [''],
         securityCode: [''],
         expirationMonth: [''],
-        expirationYear: [''],
+        expirationYear: ['']
       })
     });
+  }
+
+  copyShippingAddressToBillingAddress(event) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+            .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    }
+    else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+    
   }
 
   onSubmit() {
