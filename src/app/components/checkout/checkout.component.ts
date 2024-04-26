f9eedb8e71ac6da689146f93b26d1555e2d1332a
VitorLima <vitorliima18@gmail.com>
2024-04-26 14:54:20 -0300

    feature/CheckoutFormLayout: Added Shipping Address layout form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -7,28 +7,55 @@ import { last } from 'rxjs';
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
-export class CheckoutComponent implements OnInit{
-  
-  
+export class CheckoutComponent implements OnInit {
+
+
   checkoutFormGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){}
-  
+  constructor(private formBuilder: FormBuilder) { }
+
   ngOnInit(): void {
 
-   this.checkoutFormGroup = this.formBuilder.group({
+    this.checkoutFormGroup = this.formBuilder.group({
 
-    customer: this.formBuilder.group({
+      customer: this.formBuilder.group({
 
-      firstName: [''],
-      lastName: [''],
-      email: ['']
+        firstName: [''],
+        lastName: [''],
+        email: ['']
 
-    })
-
-   });
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCart: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        creditNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
+      })
+    });
   }
 
-  
+  onSubmit() {
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer').value);
+    console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+
+  }
+
 
 }
