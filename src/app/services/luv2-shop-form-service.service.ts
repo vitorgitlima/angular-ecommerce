5e9d229312412596050492e8dacf219ae88a8205
VitorLima <vitorliima18@gmail.com>
2024-04-27 22:09:51 -0300

    feature/CheckoutDropDownList: Implement service to deal with Expiration date of credit card year and month

---
+++ src/app/services/luv2-shop-form-service.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Luv2ShopFormServiceService {
+
+  constructor() { }
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // builder an array for "Month" dropdown list
+    // - start at current month and loop until 12
+
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+      data.push(theMonth);
+    }
+
+    return of (data);
+
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // builder an array for "Year" dropdown List
+    // - start at current year and loop for next 10 years
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++ ) {
+      data.push(theYear);
+    }
+
+}
