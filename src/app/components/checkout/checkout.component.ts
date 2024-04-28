695c25f25c51a6e06a9c6fdd1ccf06951aa962e1
VitorLima <vitorliima18@gmail.com>
2024-04-27 22:40:58 -0300

    feature/CheckoutDropDownList: Implement method for deal with expiration month on current and future years

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -97,5 +97,30 @@ export class CheckoutComponent implements OnInit {
 
   }
 
+  handleMonthsAndYear() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
 
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+    else {
+      startMonth = 1;
+    }
+
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+
+      }
+    );
+
+  }
 }
