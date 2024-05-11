cd2464a0c3b139e94f7c88cb771e95d3d659c3e0
VitorLima <vitorliima18@gmail.com>
2024-05-11 18:17:54 -0300

    feature/CheckoutFormDroDownLis:Updated checkout component url for retrieve states

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { last } from 'rxjs';
 import { Luv2ShopFormServiceService } from '../../services/luv2-shop-form-service.service';
 import { Country } from '../../common/country';
+import { State } from '../../common/state';
+
 
 @Component({
   selector: 'app-checkout',
@@ -21,6 +23,9 @@ export class CheckoutComponent implements OnInit {
   creditCardMonths: number[] = [];
 
   countries: Country[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
     private luv2ShopFormService: Luv2ShopFormServiceService) { }
@@ -134,4 +139,31 @@ export class CheckoutComponent implements OnInit {
     );
 
   }
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.luv2ShopFormService.getStates(countryCode).subscribe(
+      data => {
+
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
+        }
+        else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup.get('state').setValue(data[0]);
+      }
+    );
+  }
+
 }
